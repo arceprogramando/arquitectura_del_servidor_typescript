e67faf29e4a5a07c6d1153aa8143abfd81667ddc
felipe arce <arceprogramando@gmail.com>
2023-10-31 18:46:21 -0300

    🧼 Eliminate : build Directory from Version Control

--- build/router/product.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import { Router } from 'express'
-// import ProductController from '../controllers/product'
-// const router = Router()
-// const productController = new ProductController()
-// router.get('/', productController.getAllProducts)
-// export default router
--- build/services/product.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-// import ProductRepository from '../repository/product.ts';
